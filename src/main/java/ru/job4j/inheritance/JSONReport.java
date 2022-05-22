011ce3638feae1323be16f356d7021856d65eb3e
Artem Averin <artem-averin-91@mail.ru>
2022-05-23 01:02:47 +0300

    4. Переопределение [#174890]

---
+++ src/main/java/ru/job4j/inheritance/JSONReport.java
@@ -0,0 +1,12 @@
+package ru.job4j.inheritance;
+
+public class JSONReport {
+
+    public String generate(String name, String body) {
+        String lS = System.lineSeparator();
+        return "{" + lS
+                + "\t\"name\" : " + "\"" + name + "\"" + "," + lS
+                + "\t\"body\" : " + "\"" + body + "\"" + lS
+                + "}";
+    }
+}
