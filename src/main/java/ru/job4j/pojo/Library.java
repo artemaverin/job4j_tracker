b5cd4063111a5d94fc5393f239981ab0a88f7af1
Artem Averin <artem-averin-91@mail.ru>
2022-05-23 17:33:51 +0300

    3. Массивы и модели. [#174892]

---
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -0,0 +1,34 @@
+package ru.job4j.pojo;
+
+public class Library {
+    public static void main(String[] args) {
+        Book book1 = new Book("Three bears", 35);
+        Book book2 = new Book("Kolobok", 46);
+        Book book3 = new Book("Golden fish", 41);
+        Book book4 = new Book("Clean code", 2);
+        Book[] library = new Book[4];
+        library[0] = book1;
+        library[1] = book2;
+        library[2] = book3;
+        library[3] = book4;
+        for (int index = 0; index < library.length; index++) {
+            Book book = library[index];
+            System.out.println("Book's name: " + book.getName() + "," + " number of pages :" + book.getPagesCount());
+        }
+        System.out.println("--------------------//--------------------");
+        Book temp = library[0];
+        library[0] = library[3];
+        library[3] = temp;
+        for (int index = 0; index < library.length; index++) {
+            Book book = library[index];
+            System.out.println("Book's name: " + book.getName() + "," + " number of pages :" + book.getPagesCount());
+        }
+        System.out.println("--------------------//--------------------");
+        for (int index = 0; index < library.length; index++) {
+            Book book = library[index];
+            if (book.getName().equals("Clean code")) {
+                System.out.println("Book's name: " + book.getName() + "," + " number of pages :" + book.getPagesCount());
+            }
+        }
+    }
+}
