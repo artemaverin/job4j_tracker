c9428f27bb1a41374499d9fad025cb85e7254cba
Artem Averin <artem-averin-91@mail.ru>
2022-06-01 13:58:47 +0300

    7. Шаблон проектирования - Стратегия [#786]

---
+++ src/main/java/ru/job4j/trackerold/Tracker.java
@@ -0,0 +1,69 @@
+package ru.job4j.trackerold;
+
+import java.util.Arrays;
+
+public class Tracker {
+    private final Item[] items = new Item[100];
+    private int ids = 1;
+    private int size = 0;
+
+    public Item add(Item item) {
+        item.setId(ids++);
+        items[size++] = item;
+        return item;
+    }
+
+    public Item[] findAll() {
+        return Arrays.copyOf(items, size);
+    }
+
+    public Item[] findByName(String key) {
+        Item[] itemCopy = new Item[size];
+        int count = 0;
+        for (int i = 0; i < size; i++) {
+            Item element = items[i];
+            if (element.getName().equals(key)) {
+                itemCopy[count] = element;
+                count++;
+            }
+        }
+        return Arrays.copyOf(itemCopy, count);
+    }
+
+    public Item findById(int id) {
+        int index = indexOf(id);
+        return index != -1 ? items[index] : null;
+    }
+
+    private int indexOf(int id) {
+        int rsl = -1;
+        for (int index = 0; index < size; index++) {
+            if (items[index].getId() == id) {
+                rsl = index;
+                break;
+            }
+        }
+        return rsl;
+    }
+
+    public boolean replace(int id, Item item) {
+        int index = indexOf(id);
+        boolean rsl = index != -1;
+        if (rsl) {
+            item.setId(id);
+            items[index] = item;
+        }
+        return rsl;
+    }
+
+    public boolean delete(int id) {
+        int index = indexOf(id);
+        boolean rsl = index != -1;
+        if (rsl) {
+            System.arraycopy(items, index + 1, items, index, size - index - 1);
+            items[size - 1] = null;
+            size--;
+        }
+        return rsl;
+    }
+}
