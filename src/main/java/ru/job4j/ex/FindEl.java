ed18330927792d5e2a5aedd486e676fa4719d204
Artem Averin <artem-averin-91@mail.ru>
2022-06-04 00:32:04 +0300

    0.4. Пользовательские исключения. [#219367 #308756]

--- src/main/java/ru/job4j/ex/FindEl.java
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -4,7 +4,7 @@ public class FindEl {
     public static int indexOf(String[] value, String key) throws ElementNotFoundException {
         int rsl = -1;
         for (int i = 0; i < value.length; i++) {
-            if (key.equals(value[i])) {
+            if (value[i].equals(key)) {
                 rsl = i;
                 break;
             }
@@ -17,7 +17,7 @@ public class FindEl {
 
     public static void main(String[] args) {
         try {
-            System.out.println(FindEl.indexOf(new String[] {"one", "two"}, "three"));
+            System.out.println(FindEl.indexOf(new String[] {"one", "two"}, null));
         } catch (ElementNotFoundException e) {
             e.printStackTrace();
         }
