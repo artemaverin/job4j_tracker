aad71ca69f9d0167e7905f3fc2816d6451a58784
Artem Averin <artem-averin-91@mail.ru>
2022-06-04 22:56:21 +0300

    3. Замена if-else-throw на if-throw [#49895]

--- src/main/java/ru/job4j/ex/Find.java
+++ src/main/java/ru/job4j/ex/Find.java
@@ -10,7 +10,7 @@ public class Find {
 
     public static void main(String[] args) {
         String[] data = {"one", "two", "three"};
-        String rsl = Find.get(data, 3);
+        String rsl = Find.get(data, 0);
         System.out.println(rsl);
     }
 }
