67719d2f3fc2a1f8cfb242379f7d39866286073a
Artem Averin <artem-averin-91@mail.ru>
2022-06-18 12:57:39 +0300

    1. Телефонный справочник на базе ArrayList [#41598 #313579]

--- src/test/java/ru/job4j/search/PhoneDictionaryTest.java
+++ src/test/java/ru/job4j/search/PhoneDictionaryTest.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.IsNull.nullValue;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 
 public class PhoneDictionaryTest {
     @Test
@@ -27,6 +28,6 @@ public class PhoneDictionaryTest {
                 new Person("Petr", "Arsentev", "534872", "Bryansk")
         );
         ArrayList<Person> persons = phones.find("Dima");
-        assertThat(null, Matchers.is(nullValue()));
+        assertTrue(persons.isEmpty());
     }
 }
