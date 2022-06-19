c321a3e981e3a7f865b77e672be4440209134be4
Artem Averin <artem-averin-91@mail.ru>
2022-06-20 00:08:12 +0300

    0. Set, HashSet, Iterator. [#10095 #313861]

--- src/main/java/ru/job4j/collection/UsageHashSet.java
+++ src/main/java/ru/job4j/collection/UsageHashSet.java
@@ -9,6 +9,9 @@ public class UsageHashSet {
         autos.add("BMW");
         autos.add("Volvo");
         autos.add("Toyota");
+        autos.add("BMW");
+        autos.add("Toyota");
+        autos.add("Lada");
         for (String auto : autos) {
             System.out.println(auto);
         }
