08134c66dcc354759b47561c6c416832caf472b8
Artem Averin <artem-averin-91@mail.ru>
2022-07-20 13:32:41 +0300

    5. Подсчет функции в диапазоне. [#24251 #323202]

--- src/main/java/ru/job4j/function/FunctionCalculation.java
+++ src/main/java/ru/job4j/function/FunctionCalculation.java
@@ -8,7 +8,7 @@ public class FunctionCalculation {
     public List<Double> diapason(int start, int end, Function<Double, Double> func) {
         List<Double> res = new ArrayList<>();
         for (int i = start; i < end; i++) {
-            res.add(func.apply((double) i++));
+            res.add(func.apply((double) i));
         }
         return res;
     }
